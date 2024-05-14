73e42a222a9d496c8c52eca45376b55f739f5c3a
Leo Bufi <bufi.leo@gmail.com>
2024-05-14 14:14:42 +0200

    point-show done

---
+++ app/javascript/controllers/swiper_controller.js
@@ -0,0 +1,42 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="swiper"
+export default class extends Controller {
+  connect() {
+
+    console.log('Hi from swiper')
+
+    this.swiper = new Swiper('.swiper', {
+      // Optional parameters
+
+      direction: 'horizontal',
+      slidesPerView: 1,
+      centeredSlides: true,
+      allowSlideNext: true,
+      allowSlidePrev: true,
+
+      keyboard: {
+        enabled: true,
+      },
+
+      effect: 'fade',
+      fadeEffect: {
+        crossFade: true
+      },
+
+      // If we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+      },
+
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+
+    this.swiper.attachEvents();
+
+  }
+}
--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,6 @@ pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "jquery" # @2.2.4
+pin "swiper.js" # @1.0.0
+pin "swiper" # @11.1.3
