88e8dea6f7b895ae9573ec804934cf0813a0096e
Leo Bufi <bufi.leo@gmail.com>
2024-05-29 12:42:15 +0200

    adding import "@nathanvda/cocoon"

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "controllers"
 import "jquery";
-import "cocoon";
+// import "cocoon";
+import "@nathanvda/cocoon"
 import "@hotwired/turbo-rails"
 
 import "trix"
